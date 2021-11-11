0962feb82483254c6ffd9aef77c32fc5e1731fa6
Tim Stumper <tjstumpy@udel.edu>
2021-11-11 11:26:15 -0500

    navbar update, dark theme, font awesome

--- js/index.js
+++ js/index.js
@@ -10,6 +10,87 @@ function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 39.679811832086635, lng: -75.75088279192195 },
     zoom: 12,
+    zoom: 12,
+      styles: [
+        { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+        { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+        { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+        {
+          featureType: "administrative.locality",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }],
+        },
+        {
+          featureType: "poi",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }],
+        },
+        {
+          featureType: "poi.park",
+          elementType: "geometry",
+          stylers: [{ color: "#263c3f" }],
+        },
+        {
+          featureType: "poi.park",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#6b9a76" }],
+        },
+        {
+          featureType: "road",
+          elementType: "geometry",
+          stylers: [{ color: "#38414e" }],
+        },
+        {
+          featureType: "road",
+          elementType: "geometry.stroke",
+          stylers: [{ color: "#212a37" }],
+        },
+        {
+          featureType: "road",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#9ca5b3" }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry",
+          stylers: [{ color: "#746855" }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry.stroke",
+          stylers: [{ color: "#1f2835" }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#f3d19c" }],
+        },
+        {
+          featureType: "transit",
+          elementType: "geometry",
+          stylers: [{ color: "#2f3948" }],
+        },
+        {
+          featureType: "transit.station",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }],
+        },
+        {
+          featureType: "water",
+          elementType: "geometry",
+          stylers: [{ color: "#17263c" }],
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#515c6d" }],
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.stroke",
+          stylers: [{ color: "#17263c" }],
+        },
+      ],
   });
   infoWindow = new google.maps.InfoWindow();
   // Try HTML5 geolocation.
@@ -39,7 +120,7 @@ function initMap() {
     //clears sidebar when you click on anything but a marker
     map.addListener("click", () =>{
       content.innerHTML = '';
-      document.getElementById("locationTitle").innerText = "Click a marker to get started";
+      document.getElementById("locationTitle").innerText = "Select a marker";
       document.getElementById("locationNavbar").style.display = "none";
     });
 
@@ -79,8 +160,10 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   );
   infoWindow.open(map);
 }
+
+
 function addMarker(coordinates,placeMap){
-  mark= new google.maps.Marker({
+  mark = new google.maps.Marker({
     position: coordinates,
     map: placeMap,
   });
