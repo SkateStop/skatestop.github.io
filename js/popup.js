b51ff2358db89102ae9b30d5fb102485aad8c88c
Tim Stumper <tjstumpy@udel.edu>
2021-11-18 10:25:26 -0500

    updated 'add location' css

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,7 @@
 function openForm() {
-    document.getElementById("myForm").style.display = "block";
-  }
+  document.getElementById("myForm").style.display = "inline";
+}
   
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
