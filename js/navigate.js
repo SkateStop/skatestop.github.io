10b864c737354630d22e3c711dfec6a4fa165044
Tim Stumper <tjstumpy@udel.edu>
2021-12-09 10:24:20 -0500

    fixed undefined bug, added addMedia button

--- js/navigate.js
+++ js/navigate.js
@@ -5,14 +5,6 @@ let reviewBtn = document.querySelector("#reviewBtn");
 let dirBtn = document.querySelector("#dirBtn");
 let content = document.querySelector(".navbarDisplay");
 
-
-
-reviewBtn.addEventListener('click', () => {
+/* reviewBtn.addEventListener('click', () => {
     content.innerHTML = 'no reviews yet...';
-});
-
-dirBtn.addEventListener('click', () => {
-    content.innerHTML = '<input type="button" value="Open in Google Maps" class="directionButton">';
-});
-
-//PHILLIPS PARK
+}); */
